d8e8be48b42e486a2508d54a99446e1039032c88
H P <humanpizzza@gmail.com>
2021-12-03 22:20:34 +0300

    add progressbar

--- page_loader/downloader.py
+++ page_loader/downloader.py
@@ -7,17 +7,16 @@ import logging.config
 import logging
 from page_loader.logging import LOGGING_CONFIG
 from pathlib import Path
-from urllib.error import HTTPError
-import sys
+from progress.bar import FillingSquaresBar
+#from urllib.error import HTTPError
+#import sys
 
-#A good convention to use when naming loggers is to use a module-level logger,
-#  in each module which uses logging, named as follows:
-#
+
 logging.config.dictConfig(LOGGING_CONFIG)
 logger = logging.getLogger('page_loader')
 
+
 def download(original_url, path=''):
-    #logger = logging.getLogger('page_loader')
     logger.info(f'Download {original_url}...')
     if not os.path.exists(path):
         os.makedirs(path)
@@ -34,25 +33,27 @@ def download(original_url, path=''):
 
 def download_resources(original_url, local_dir):
     logger.info(f'Saving to the {local_dir}')
-    #try:
     root_dir = make_folder(original_url,local_dir)
     logger.info(f'Create directory: {root_dir}')
     urls, _ = update_links(get_response(original_url).content, original_url, local_dir)
     logger.info(f'Download resources...')
+    bar = FillingSquaresBar('Loading', max=len(urls))
     for item in urls:
         url = get_response(item['url']).content
         local_path =  os.path.join(root_dir, str(item['filename']))
         make_save(local_path, url)
+        bar.next()
+    bar.finish()
 
 
 def make_save(path_to_file, data):
-    try:
-        with open(path_to_file, 'wb') as f:
-            logger.info(f'Save to the {path_to_file}')
-            f.write(data)
-    except PermissionError:
-        logger.error(f'Permission denied')
-        raise
+    #try:
+    with open(path_to_file, 'wb') as f:
+        logger.info(f'Save to the {path_to_file}')
+        f.write(data)
+    #except PermissionError:
+        #logger.error(f'Permission denied')
+        #raise
 
 #OSError
 def get_response(url):
