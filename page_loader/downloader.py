c6d2ffdedecf14a28404e9065eb823675b7b0f51
H P <humanpizzza@gmail.com>
2022-03-07 11:59:18 +0300

    change fix logger v12

--- page_loader/downloader.py
+++ page_loader/downloader.py
@@ -23,6 +23,7 @@ def download(original_url, path=''):
         raise Error(f'Incorrect folder: {path}') from FileNotFoundError
     path_html = os.path.join(path, rename_filename(original_url))
     local_path = os.path.join(path, get_folder_name(original_url))
+    logger.debug(f'Downloading  from {original_url} to {path_html}')
     urls, html = update_links(get_response(original_url).text,
                               original_url, local_path)
     make_save(path_html, html)
@@ -51,7 +52,6 @@ def download_resources(original_url, local_dir, urls):
 
 def get_response(url):
     try:
-        logger.info('Downloading {url}')
         response = requests.get(url, stream=True)
         response.raise_for_status()
         return response
